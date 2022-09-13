d5e0fd87fa9050bf3220abd6da38b74f08769602
emrah.ozturk <emrah.ozturk@fintelum.com>
2022-09-13 20:48:22 +0300

    docker fix

--- cargoService/src/main/resources/data/changelog_1.0.sql
+++ cargoService/src/main/resources/data/changelog_1.0.sql
@@ -3,3 +3,6 @@ INSERT INTO cargo (id,order_id,courier_id,cargo_status) values (1,1,3,'ASSIGNED'
 
 create table "courier" ("id" bigserial not null, "name" varchar(255) not null, "courier_status" varchar(255), primary key ("id"));
 INSERT INTO courier (id,name, courier_status) values (1,'courier1', 'FREE');
+
+ALTER SEQUENCE cargo_id_seq RESTART WITH 2;
+ALTER SEQUENCE courier_id_seq RESTART WITH 2;
